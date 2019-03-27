115df39c0a9451008cf75572ef771be0d4968388
daodao <188037271@qq.com>
2019-03-27 17:23:01 +0800

    111

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int a[] = new int[50];
-        for (int i = 0; i < 50; ++i){
+        int a[] = new int[20];
+        for (int i = 0; i < 20; ++i){
             a[i] = (int) (Math.random() * (1000 - 1) + 1);
 
         }
         System.out.println(Arrays.toString(a));
-        bubbleSort bubbleSort = new bubbleSort();
-        a =  bubbleSort.sort(a);
+      //  bubbleSort bubbleSort = new bubbleSort();
+       // a =  bubbleSort.sort(a);
+        insertSort insertSort = new insertSort();
+        a = insertSort.sort(a);
         System.out.println(Arrays.toString(a));
     }
 }
---
+++ src/insertSort.java
@@ -0,0 +1,19 @@
+public class insertSort implements Sort {
+    @Override
+    public int[] sort(int[] array) {
+        int length = array.length;
+        if (length <= 1) return array;
+        for (int i = 1; i < length; ++i) {
+            int value = array[i];
+            int j = i - 1;
+            for (; j >= 0; --j) {
+                if (array[j] > value) {
+                    array[j + 1] = array[j];
+                }
+                else {break;}
+            }
+            array[j + 1] = value;
+        }
+        return array;
+    }
+}
